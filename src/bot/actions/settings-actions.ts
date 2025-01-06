602e3a288103216b1ca84d1c13e0c7cbf8b41413
diffurchik <de.Aliatta@gmail.com>
2025-01-06 18:07:46 +0100

    main fix bugs with settings

--- src/bot/actions/settings-actions.ts
+++ src/bot/actions/settings-actions.ts
@@ -13,113 +13,150 @@ import {ActionSteps, MyContext, UserStatesType} from "../../types";
 export const settingsActions = (bot: Telegraf<MyContext>, userActionState: UserStatesType) => {
 
     bot.action('SETTINGS', async (ctx: Context) => {
-        const {userId} = getUserData(ctx)
-        if (userId) {
-            const userSchedule = await getScheduleByUser(userId)
-            if (userSchedule && userSchedule.length > 0) {
-                const {show_random_card, rand_card_time, reminder_time, send_reminder} = userSchedule[0]
-                const text: string = `Your current settings is: \n\n` +
-                    `▪️Send a random card daily: ${show_random_card ? `✅` : `No`}\n` +
-                    `▪️Time to send a random card: *${rand_card_time ? rand_card_time : 'No'}*\n ` +
-                    `▪️Send a study reminder daily: ${send_reminder ? '✅' : 'No'}\n ` +
-                    `▪️Time to send a study reminder: *${reminder_time ? reminder_time : 'No'}*\n `
-                await ctx.editMessageText(text, {reply_markup: settingsMenu(), parse_mode: "MarkdownV2"})
-            } else {
-                const text: string = `Your current settings is: \n\n ▪️Send a random card daily: No\n ▪️Time to send a random card: No`
-                await ctx.editMessageText(text, {reply_markup: settingsMenu(), parse_mode: "MarkdownV2"})
+        try {
+            const {userId} = getUserData(ctx)
+            if (userId) {
+                const userSchedule = await getScheduleByUser(userId)
+                if (userSchedule && userSchedule.length > 0) {
+                    const {show_random_card, rand_card_time, reminder_time, send_reminder} = userSchedule[0]
+                    const text: string = `Your current settings is: \n\n` +
+                        `▪️Send a random card daily: ${show_random_card ? `✅` : `No`}\n` +
+                        `▪️Time to send a random card: *${rand_card_time ? rand_card_time : 'No'}*\n ` +
+                        `▪️Send a study reminder daily: ${send_reminder ? '✅' : 'No'}\n ` +
+                        `▪️Time to send a study reminder: *${reminder_time ? reminder_time : 'No'}*\n `
+                    await ctx.editMessageText(text, {reply_markup: settingsMenu(), parse_mode: "MarkdownV2"})
+                } else {
+                    const text: string = `Your current settings is: \n\n` +
+                        `▪️Send a random card daily: *No*\n` +
+                        `▪️Time to send a random card: *No*\n ` +
+                        `▪️Send a study reminder daily: *No*\n ` +
+                        `▪️Time to send a study reminder: *No*\n `
+                    await ctx.editMessageText(text, {reply_markup: settingsMenu(), parse_mode: "MarkdownV2"})
+                }
             }
+        } catch (error) {
+            console.log(error);
         }
     })
 
     bot.action('SET_SENDING_RANDOM_CARD', async (ctx: Context) => {
-        const {userId} = getUserData(ctx)
-        if (userId) {
-            const userSchedule = await getScheduleByUser(userId)
-            if (userSchedule && userSchedule.length > 0) {
-                const {show_random_card, rand_card_time} = userSchedule[0]
-                await updateShowRandomCardDaily(userId, !show_random_card)
-                const text: string = !show_random_card ? `You will get a random card daily at ${rand_card_time}` : 'You will not get a random card daily'
-                await ctx.editMessageText(text, {reply_markup: settingsRandomCard(!show_random_card)})
-            } else {
-                await insertRandomCardTime(userId, '09:00', true)
-                const text: string = `You will get a random card daily at 09:00 You can change this time in the settings`
-                await ctx.editMessageText(text, {reply_markup: settingsRandomCard(true)})
+        try {
+            const {userId} = getUserData(ctx)
+            if (userId) {
+                const userSchedule = await getScheduleByUser(userId)
+                if (userSchedule && userSchedule.length > 0) {
+                    const {show_random_card, rand_card_time} = userSchedule[0]
+                    await updateShowRandomCardDaily(userId, !show_random_card)
+                    const text: string = !show_random_card ? `You will get a random card daily at ${rand_card_time}` : 'You will not get a random card daily'
+                    await ctx.editMessageText(text, {reply_markup: settingsRandomCard(!show_random_card)})
+                } else {
+                    await insertRandomCardTime(userId, '09:00', true)
+                    const text: string = `You will get a random card daily at 09:00 You can change this time in the settings`
+                    await ctx.editMessageText(text, {reply_markup: settingsRandomCard(true)})
+                }
             }
+        } catch (error) {
+            console.log(error);
         }
+
     })
 
     bot.action('SET_RANDOM_CARD_TIME', async (ctx: Context) => {
-        const {userId, username} = getUserData(ctx)
-        await ctx.reply("At what time (HH:MM, 24-hour format) should I send you a random card daily?", {reply_markup: {force_reply: true}})
-        if (userId) {
-            userActionState[userId] = {username: username, step: ActionSteps.SetRandomTime};
+        try {
+            const {userId, username} = getUserData(ctx)
+            await ctx.reply("At what time (HH:MM, 24-hour format) should I send you a random card daily?", {reply_markup: {force_reply: true}})
+            if (userId) {
+                userActionState[userId] = {username: username, step: ActionSteps.SetRandomTime};
+            }
+        } catch (error) {
+            console.log(error);
         }
     })
 
     bot.action('SETTINGS_RANDOM_CARD', async (ctx: Context) => {
-        const {userId} = getUserData(ctx)
-        if (userId) {
-            const userSchedule = await getScheduleByUser(userId)
-            if (userSchedule && userSchedule.length > 0) {
-                const {show_random_card, rand_card_time} = userSchedule[0]
-                const text: string = `Your current settings is: \n\n ▪️Send a random card daily: ${show_random_card ? `✅` : `No`}\n ▪️Time to send a random card: *${rand_card_time}*`
-                await ctx.editMessageText(text, {
-                    reply_markup: settingsRandomCard(show_random_card),
-                    parse_mode: "MarkdownV2"
-                })
-            } else {
-                const text: string = `Your current settings is: \n\n ▪️Send a random card daily: No\n ▪️Time to send a random card: No`
-                await ctx.editMessageText(text, {reply_markup: settingsRandomCard()})
+        try {
+            const {userId} = getUserData(ctx)
+            if (userId) {
+                const userSchedule = await getScheduleByUser(userId)
+                if (userSchedule && userSchedule.length > 0) {
+                    const {show_random_card, rand_card_time} = userSchedule[0]
+                    const text: string = `Your current settings is: \n\n ▪️Send a random card daily: ${show_random_card ? `✅` : `No`}\n ▪️Time to send a random card: *${rand_card_time}*`
+                    await ctx.editMessageText(text, {
+                        reply_markup: settingsRandomCard(show_random_card),
+                        parse_mode: "MarkdownV2"
+                    })
+                } else {
+                    const text: string = `Your current settings is: \n\n ▪️Send a random card daily: No\n ▪️Time to send a random card: No`
+                    await ctx.editMessageText(text, {reply_markup: settingsRandomCard()})
+                }
             }
+        } catch (error) {
+            console.log(error);
         }
+
     })
 
     bot.action('SETTINGS_REMINDER', async (ctx: Context) => {
-        const {userId} = getUserData(ctx)
-        if (!userId) return
-        const userSchedule = await getScheduleByUser(userId)
-        if (userSchedule && userSchedule.length > 0) {
-            const {send_reminder, reminder_time} = userSchedule[0]
-            const text: string = `Your current settings is: \n\n ▪️Send a reminder daily: ${send_reminder ? `✅` : `No`}\n ▪️Time to send a reminder: *${reminder_time}*`
-            await ctx.editMessageText(text, {reply_markup: settingsReminder(), parse_mode: "MarkdownV2"})
-        } else {
-            const text: string = `Your current settings is: \n\n ▪️Send a random card daily: No\n ▪️Time to send a random card: No`
-            await ctx.editMessageText(text, {reply_markup: settingsReminder()})
+        try {
+            const {userId} = getUserData(ctx)
+            if (!userId) return
+            const userSchedule = await getScheduleByUser(userId)
+            if (userSchedule && userSchedule.length > 0) {
+                const {send_reminder, reminder_time} = userSchedule[0]
+                const text: string = `Your current settings is: \n\n ▪️Send a reminder daily: ${send_reminder ? `✅` : `No`}\n ▪️Time to send a reminder: *${reminder_time}*`
+                await ctx.editMessageText(text, {reply_markup: settingsReminder(), parse_mode: "MarkdownV2"})
+            } else {
+                const text: string = `Your current settings is: \n\n ▪️Send a random card daily: No\n ▪️Time to send a random card: No`
+                await ctx.editMessageText(text, {reply_markup: settingsReminder()})
+            }
+        } catch (error) {
+            console.log(error);
         }
+
 
     })
 
     bot.action('SET_SENDING_REMINDER', async (ctx: Context) => {
-        const {userId} = getUserData(ctx)
-        if (!userId) return
+        try {
+            const {userId} = getUserData(ctx)
+            if (!userId) return
 
-        const userSchedule = await getScheduleByUser(userId)
-        const hasSchedule = userSchedule && userSchedule.length > 0;
-        const {send_reminder, reminder_time} = hasSchedule ? userSchedule[0] : {};
+            const userSchedule = await getScheduleByUser(userId)
+            const hasSchedule = userSchedule && userSchedule.length > 0;
+            const {send_reminder, reminder_time} = hasSchedule ? userSchedule[0] : {};
 
-        const newSendReminder = !send_reminder;
-        const reminderTime = reminder_time || '09:00';
+            const newSendReminder = !send_reminder;
+            const reminderTime = reminder_time || '09:00';
 
-        if (!hasSchedule || !reminder_time) {
-            if (!hasSchedule) await insertReminderTime(userId, reminderTime, newSendReminder);
-            else await updateReminderTime(userId, reminderTime);
+            if (!hasSchedule || !reminder_time) {
+                if (!hasSchedule) await insertReminderTime(userId, reminderTime, newSendReminder);
+                else await updateReminderTime(userId, reminderTime);
+            }
+
+            await updateSendReminder(userId, newSendReminder);
+
+            const text = newSendReminder
+                ? `You will get a reminder daily at ${reminderTime}. You can change this time in the settings.`
+                : 'You will not get a random card daily.';
+
+            await ctx.editMessageText(text, {reply_markup: settingsReminder(newSendReminder)});
+        } catch (error) {
+            console.log(error);
         }
 
-        await updateSendReminder(userId, newSendReminder);
-
-        const text = newSendReminder
-            ? `You will get a reminder daily at ${reminderTime}. You can change this time in the settings.`
-            : 'You will not get a random card daily.';
-
-        await ctx.editMessageText(text, {reply_markup: settingsReminder(newSendReminder)});
 
     })
 
     bot.action('SET_REMINDER_TIME', async (ctx: Context) => {
-        const {userId, username} = getUserData(ctx)
-        await ctx.reply("At what time (HH:MM, 24-hour format) should I send you a random card daily? For example: 11:30", {reply_markup: {force_reply: true}})
-        if (userId) {
-            userActionState[userId] = {username: username, step: ActionSteps.SetReminderTime};
+        try {
+            const {userId, username} = getUserData(ctx)
+            await ctx.reply("At what time (HH:MM, 24-hour format) should I send you a random card daily? For example: 11:30", {reply_markup: {force_reply: true}})
+            if (userId) {
+                userActionState[userId] = {username: username, step: ActionSteps.SetReminderTime};
+            }
+        } catch (error) {
+            console.log(error);
         }
+
     })
 }
